220db02792a1b83e282dbc2b3c47289fb6b490a0
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-08-09 17:18:38 +0800

    Create planting-a-tree.java

---
+++ learning-sandbox/planting-a-tree.java
@@ -0,0 +1,12 @@
+public class Tree {
+  public static void main(String[] args) {
+    /* We're going to introduce ourselves and print a tree to the screen. */
+    System.out.println("Hello there! I'm Abe and am learning to code in Java.");
+    System.out.println("I'm going to plant a tree today. Ready to get my hands a little dirty!");
+    System.out.println(" ***  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  ");
+  }
+}
