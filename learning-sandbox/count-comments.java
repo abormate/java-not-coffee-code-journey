b6ea2e8d79f96ddda401ee0544afe3ccee504de4
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-08-10 17:43:06 +0800

    Create count-comments.java

---
+++ learning-sandbox/count-comments.java
@@ -0,0 +1,15 @@
+//This is the class declaration
+// Another comment 
+public class CountComment {
+  //This is the main method that runs when you compile
+	public static void main(String[] args) {
+    //This is where you will define your variable
+    int numComments = 7;
+    //This is where you will print your variable
+    System.out.println("There's around " + numComments + " comments...");
+	}
+  
+  //This is the end of the class
+}
+
+//This is outside the class
